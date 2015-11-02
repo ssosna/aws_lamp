c2b643228c3686225e99a65fca8d0fd8743c8acb
ssosna <sergio.sosna@gmail.com>
2015-11-02 18:32:44 +0200

    edit lamp script

--- lamp_setup.pp
+++ lamp_setup.pp
@@ -1,3 +1,8 @@
+exec { "sudoers":
+    path => ["/etc/","/usr/sbin/","/bin"],
+    command => "sed -i 's/"Defaults requiretty"/"#Defaults requiretty"/g' sudoers",
+    user => root,
+}
 file { '/tmp/111.conf':
         ensure => file,
         owner  => 'root',
